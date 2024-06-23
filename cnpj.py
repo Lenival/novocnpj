e2cb45d9a8275e5ebdebd4a716cc4d30b6b73cae
Lenival <lenival@ufrn.edu.br>
2024-06-23 19:39:26 -0300

    Remoção e dependência e mais
    Removida a dependência do numpy usando o int básico do Python. Retornando diretamente o int convertido por str quando o cnpj inteiro é menor que o máximo do antigo.

--- cnpj.py
+++ cnpj.py
@@ -7,9 +7,7 @@ __author__ = "Lenival Gomes"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import numpy as np
-
-MAX_CNPJ_ANTIGO = (np.uint64(10)**np.uint64(14)-np.uint64(1))
+MAX_CNPJ_ANTIGO = (int(10)**int(14)-int(1))
 
 
 """
@@ -22,28 +20,18 @@ def to_uint64(cnpj_string):
         raise Exception("Não alfanumérico")
     
     cnpj_string_u = cnpj_string.upper()
-    # print(cnpj_string_u)
-    # print(cnpj_string_u[::-1])
     
     # Inicialização de acumuladores multiplicativo e aditivo
-    acum_m = np.uint64(1)
-    acum_a = np.uint64(0)
+    acum_m = int(1)
+    acum_a = int(0)
     acum_a += (0 if str.isnumeric(cnpj_string) else MAX_CNPJ_ANTIGO)
     
     # Seleciona a base 10 para CNPJ antigo e 36 para novos
-    base = np.uint64(10 if str.isnumeric(cnpj_string) else 36)
+    base = int(10 if str.isnumeric(cnpj_string) else 36)
     
     for alfanum in list(cnpj_string_u[::-1]):
-        #print(alfanum)
-        #print(ord(alfanum))
-        ##print(ord(alfanum)-48)
-        #print(ord(alfanum)-48-(7 if ord(alfanum)>=65 else 0))
-        acum_a += np.uint64(acum_m*np.uint64(ord(alfanum)-48-(7 if ord(alfanum)>=65 else 0)))
-        #print(acum_a)
+        acum_a += int(acum_m*int(ord(alfanum)-int(48)-int(7 if ord(alfanum)>=65 else 0)))
         acum_m *=base
-        #print(acum_m)
-        #print(type(acum_m))
-        #print(type(acum_a))
     
     # print(int(acum_m))
     print(f'O valor inteiro para {cnpj_string} é {int(acum_a)}')
@@ -53,27 +41,24 @@ def to_uint64(cnpj_string):
 to_string recebe um uint64 e retorna a string do CNPJ correspondente.
 """
 def to_string(cnpj_uint64):
-    if not isinstance(cnpj_uint64, np.uint64):
+    if not isinstance(cnpj_uint64, int):
         raise Exception("Não é um uint64")
-        
-    if cnpj_uint64 <= MAX_CNPJ_ANTIGO:
-        (base,expoente,resto) = (np.uint64(10),np.uint64(13),cnpj_uint64)
-    else:
-        (base,expoente,resto) = (np.uint64(36),np.uint64(11),cnpj_uint64-MAX_CNPJ_ANTIGO)
-        
-    acum_m = np.uint64(base**np.uint64(expoente))
-    quociente = np.uint64(0)
-    cnpj_string = ''
     
-    while(acum_m != np.uint64(0)):
-        quociente = resto//acum_m
-        resto -= quociente*acum_m
-        cnpj_string = cnpj_string + (chr(quociente + np.uint64(48) + np.uint64(7 if quociente > 9 else 0)))
-        # print(f'quociente:{quociente} resto:{resto} acum_m:{acum_m} base:{base} cnpj_string:' + cnpj_string)
-        acum_m = acum_m//base
-    # print(base)
-    # print(expoente)
-    # print(acum_m) 
+    cnpj_string = ''
+    if cnpj_uint64 <= MAX_CNPJ_ANTIGO:
+        cnpj_string = str(cnpj_uint64)
+    else:
+        (base,expoente,resto) = (int(36),int(11),int(cnpj_uint64-MAX_CNPJ_ANTIGO))
+        
+        acum_m = int(base**int(expoente))
+        quociente = int(0)
+        
+        while(acum_m != int(0)):
+            quociente = resto//acum_m
+            resto -= quociente*acum_m
+            deslocamento_ascii = int((int(7) if quociente > 9 else int(0)))
+            cnpj_string = cnpj_string + (chr( int(quociente + int(48) + deslocamento_ascii)))
+            acum_m = acum_m//base
     print(f'O CNPJ uint64 {cnpj_uint64} como string é: '+cnpj_string)
 
 
@@ -93,11 +78,11 @@ def main():
     #to_string(np.uint64(1)+np.uint64(10)**np.uint64(14))
     #to_string(np.uint64(10)**np.uint64(14))
     #to_string(np.uint64(4029188161716827808))
-    to_string(np.uint64(4029288161716827807)) # UM1000DEPEAO
-    to_string(np.uint64(4738481338321616894)) # ZZZZZZZZZZZZ
-    to_string(np.uint64(123456789011)) # 123456789011
-    to_string(np.uint64(12345678901112)) # 123456789011
-    to_string(np.uint64(100000000000009)) # 00000000000A
+    to_string(int(4029288161716827807)) # UM1000DEPEAO
+    to_string(int(4738481338321616894)) # ZZZZZZZZZZZZ
+    to_string(int(123456789011)) # 123456789011
+    to_string(int(12345678901112)) # 123456789011
+    to_string(int(100000000000009)) # 00000000000A
     
     
 
